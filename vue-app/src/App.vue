b8658dec17febe801aaff2fc456c38f9b2f759d0
Diego Parra <diegoparraferreira@gmail.com>
2022-03-18 15:04:32 -0400

    updated

---
+++ vue-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<Header />
+	<Body />
+	<Footer />
+</template>
+
+<script>
+import Header from "./components/Header.vue"
+import Body from "./components/Body.vue"
+import Footer from "./components/Footer.vue"
+export default {
+	name: 'App',
+	components: {
+		Header,
+		Body,
+		Footer,
+	}
+}
+</script>
+
+<style>
+
+</style>
