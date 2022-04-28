6b1e4b4c5d946bc66e04f70040d625eb918399d6
Diego Parra <diegoparraferreira@gmail.com>
2022-04-28 15:12:04 -0400

    updates

--- vue-app/src/components/Login.vue
+++ vue-app/src/components/Login.vue
@@ -1,13 +1,58 @@
 <template>
-	<div class="container">
-		<div class="row">
-			<div class="col">
-				<h1 class="mt-5"> Login</h1>
-				<hr>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="mt-5">Login</h1>
+        <hr />
+        <form method="post" action="/login" class="needs-validation" novalidate>
+          <text-input label="Email" type="email" name="email" required="true">
+          </text-input>
 
-				<hr>
-				<input type="submit" class="btn btn-primary" value="Login">
-			</div>
-		</div>
-	</div>
+          <text-input
+            label="Password"
+            type="password"
+            name="password"
+            required="true"
+          >
+          </text-input>
+          <hr />
+          <input type="submit" class="btn btn-primary" value="Login" />
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import TextInput from "./forms/textInput.vue";
+export default {
+  name: "losin",
+  components: {
+    TextInput,
+  },
+  mounted() {
+    (function () {
+      "use strict";
+
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.querySelectorAll(".needs-validation");
+
+      // Loop over them and prevent submission
+      Array.prototype.slice.call(forms).forEach(function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (!form.checkValidity()) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    })();
+  },
+};
+</script>
---
+++ vue-app/src/components/forms/checkInput.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="form-check">
+      <input class="form-check-input" :checked="checked" :required="required" type="checkbox" :value="value" :name="name" :id="name">
+      <label class="form-check-label" :for="name">
+         {{label}}
+      </label>
+      </input>
+   </div>
+</template>
+
+<script>
+export default {
+  name: "CheckInput",
+  props: ["label", "required", "name", "value", "checked"],
+};
+</script>
